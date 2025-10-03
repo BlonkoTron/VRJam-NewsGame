5b28570ad62904ce70f49d18c760fe4a4a0dd07e
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-10-03 14:19:46 +0200

    Dynamic zoom with boxcast

--- Assets/BoxRayCast.cs
+++ Assets/BoxRayCast.cs
@@ -3,26 +3,23 @@ using UnityEngine;
 public class BoxRayCast : MonoBehaviour
 {
     [Header("BoxCast Settings")]
-    public Vector3 halfExtents = new Vector3(0.1f, 0.1f, 0.1f); //Box size
+    public Vector3 halfExtents = new Vector3(0.5f, 0.5f, 0.5f); //Box size
     public float maxDistance = 10f;
     public float hitdistance;
     public LayerMask hitLayers;     // Layerhit
 
+    public Camera Handcam;
+    public float Distancebox;
     void Update()
     {
         Vector3 direction = transform.forward;
-
+        Distancebox = (Handcam.fieldOfView - 40) / 5;
         // BoxCast
         if (Physics.BoxCast(transform.position, halfExtents, direction, out RaycastHit hit, transform.rotation, maxDistance, hitLayers))
         {
-            hitdistance = hit.distance;
+            hitdistance = maxDistance;
             Debug.DrawRay(transform.position, direction * hitdistance, Color.red);
             //Debug.Log("Hit: " + hit.collider.name + " at distance " + hitdistance);
-        }
-        else
-        {
-            hitdistance = maxDistance;
-            Debug.DrawRay(transform.position, direction * maxDistance, Color.red);
         }
     }
 
@@ -31,6 +28,6 @@ public class BoxRayCast : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
-        Gizmos.DrawWireCube(Vector3.forward * hitdistance, halfExtents * 2);
+        Gizmos.DrawWireCube(Vector3.forward * Distancebox, halfExtents * 2);
     }
 }
