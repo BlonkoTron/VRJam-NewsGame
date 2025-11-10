77955e1f9c42f9bf10deece148d1865380f1d7b6
oscarbagger <oscarbagger@live.dk>
2025-11-10 12:52:52 +0100

    exploding pigeons

--- Assets/!GAME/Scripts/BirdFollowPlayer.cs
+++ Assets/!GAME/Scripts/BirdFollowPlayer.cs
@@ -4,7 +4,10 @@ public class BirdFollowPlayer : MonoBehaviour
 {
     [SerializeField] private float speed = 2;
     [SerializeField] private float minDistanceToStopMoving=0.1f;
+    [SerializeField] private GameObject explodeParticlePrefab;
     private Transform target;
+
+    private bool hasBeenGrabbed=false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,6 +22,21 @@ public class BirdFollowPlayer : MonoBehaviour
             // Move our position a step closer to the target.
             float step = speed * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            transform.LookAt(target.position);
+            transform.Rotate(new Vector3(0, 90, 0));
         }
     }
+    public void SetHasBeenGrabbed(bool grab)
+    {
+        hasBeenGrabbed = grab;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hasBeenGrabbed)
+        {
+            Instantiate(explodeParticlePrefab,transform.position,Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+
 }
