554ecbb283a3e097d602511c0813be38c93442b7
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-11-13 13:56:50 +0100

    COOLSCRIPT

--- Assets/!GAME/Scripts/Ranksystem.cs
+++ Assets/!GAME/Scripts/Ranksystem.cs
@@ -3,7 +3,7 @@ using UnityEngine.Video;
 
 public class Ranksystem : MonoBehaviour
 {
-    public VideoPlayer Videoplayer;
+    public VideoPlayer Videoplayers;
 
     public VideoClip S_Rank;
     public VideoClip A_Rank;
@@ -13,14 +13,22 @@ public class Ranksystem : MonoBehaviour
     public VideoClip E_Rank;
     public VideoClip F_Rank;
 
+    public bool S_rankCheck;
+    public bool A_rankCheck;
+    public bool B_rankCheck;
+    public bool C_rankCheck;
+    public bool D_rankCheck;
+    public bool E_rankCheck;
+    public bool F_rankCheck;
+
     public VideoClip FinalRank;
-    public float S_Rank_Threshold;
-    public float A_Rank_Threshold;
-    public float B_Rank_Threshold;
-    public float C_Rank_Threshold;
-    public float D_Rank_Threshold;
-    public float E_Rank_Threshold;
-    public float F_Rank_Threshold;
+    public float S_Rank_PointThreshold;
+    public float A_Rank_PointThreshold;
+    public float B_Rank_PointThreshold;
+    public float C_Rank_PointThreshold;
+    public float D_Rank_PointThreshold;
+    public float E_Rank_PointThreshold;
+    public float F_Rank_PointThreshold;
 
     public PointManager Pointmanagers;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -32,32 +40,40 @@ public class Ranksystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Pointmanagers.totalPoints < F_Rank_Threshold)
+        if (Pointmanagers.totalPoints <= F_Rank_PointThreshold && (F_rankCheck == false))
         {
             FinalRank = F_Rank;
-        } else if (Pointmanagers.totalPoints < E_Rank_Threshold)
+            F_rankCheck = true;
+        } else if (Pointmanagers.totalPoints <= E_Rank_PointThreshold && (E_rankCheck == false))
         {
             FinalRank = E_Rank;
-        } else if (Pointmanagers.totalPoints < D_Rank_Threshold)
+            E_rankCheck = true;
+        } else if (Pointmanagers.totalPoints <= D_Rank_PointThreshold && (D_rankCheck == false))
         {
             FinalRank = D_Rank;
-        } else if (Pointmanagers.totalPoints < C_Rank_Threshold)
+            D_rankCheck = true;
+        } else if (Pointmanagers.totalPoints <= C_Rank_PointThreshold && (C_rankCheck == false))
         {
             FinalRank = C_Rank;
-        } else if (Pointmanagers.totalPoints < B_Rank_Threshold)
+            C_rankCheck = true;
+        } else if (Pointmanagers.totalPoints <= B_Rank_PointThreshold && (B_rankCheck == false))
         {
             FinalRank = B_Rank;
-        } else if (Pointmanagers.totalPoints < A_Rank_Threshold)
+            B_rankCheck = true;
+        } else if (Pointmanagers.totalPoints <= A_Rank_PointThreshold && (A_rankCheck == false))
         {
             FinalRank = A_Rank;
-        } else if (Pointmanagers.totalPoints < S_Rank_Threshold)
+            A_rankCheck = true;
+        } else if (Pointmanagers.totalPoints <= S_Rank_PointThreshold && (S_rankCheck == false))
         {
             FinalRank = S_Rank;
+            S_rankCheck = true;
         } 
     }
 
     public void FinalRankScore()
     {
-        //here we play the current finalrank video
+        //here we play the current finalrank video when the game is over
+        Videoplayers.clip = FinalRank;
     }
 }
