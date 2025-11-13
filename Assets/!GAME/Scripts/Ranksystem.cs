b35e5040378c8dfb6d19a6b61074e5095f54b0d2
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-11-13 13:35:55 +0100

    point

--- Assets/!GAME/Scripts/Ranksystem.cs
+++ Assets/!GAME/Scripts/Ranksystem.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Ranksystem : MonoBehaviour
 {
+    public VideoPlayer Videoplayer;
 
+    public VideoClip S_Rank;
+    public VideoClip A_Rank;
+    public VideoClip B_Rank;
+    public VideoClip C_Rank;
+    public VideoClip D_Rank;
+    public VideoClip E_Rank;
+    public VideoClip F_Rank;
+
+    public PointManager Pointmanager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Pointmanager = GameObject.Find("Pointssystem").GetComponent<PointManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
