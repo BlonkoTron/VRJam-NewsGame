553218e102ad860caade7bcee8878d6655100f2f
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-11-13 13:48:13 +0100

    rankscript

--- Assets/!GAME/Scripts/Ranksystem.cs
+++ Assets/!GAME/Scripts/Ranksystem.cs
@@ -13,6 +13,15 @@ public class Ranksystem : MonoBehaviour
     public VideoClip E_Rank;
     public VideoClip F_Rank;
 
+    public VideoClip FinalRank;
+    public float S_Rank_Threshold;
+    public float A_Rank_Threshold;
+    public float B_Rank_Threshold;
+    public float C_Rank_Threshold;
+    public float D_Rank_Threshold;
+    public float E_Rank_Threshold;
+    public float F_Rank_Threshold;
+
     public PointManager Pointmanagers;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +32,32 @@ public class Ranksystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Pointmanagers.totalPoints = 0;
+        if (Pointmanagers.totalPoints < F_Rank_Threshold)
+        {
+            FinalRank = F_Rank;
+        } else if (Pointmanagers.totalPoints < E_Rank_Threshold)
+        {
+            FinalRank = E_Rank;
+        } else if (Pointmanagers.totalPoints < D_Rank_Threshold)
+        {
+            FinalRank = D_Rank;
+        } else if (Pointmanagers.totalPoints < C_Rank_Threshold)
+        {
+            FinalRank = C_Rank;
+        } else if (Pointmanagers.totalPoints < B_Rank_Threshold)
+        {
+            FinalRank = B_Rank;
+        } else if (Pointmanagers.totalPoints < A_Rank_Threshold)
+        {
+            FinalRank = A_Rank;
+        } else if (Pointmanagers.totalPoints < S_Rank_Threshold)
+        {
+            FinalRank = S_Rank;
+        } 
+    }
+
+    public void FinalRankScore()
+    {
+        //here we play the current finalrank video
     }
 }
