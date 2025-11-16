70065e503742ac3599aa0bbbdb2270f6a19c7a87
BlonkoTron <133660863+BlonkoTron@users.noreply.github.com>
2025-11-16 15:50:39 +0100

    Update DeleteAfterAnimation.cs

--- Assets/DeleteAfterAnimation.cs
+++ Assets/DeleteAfterAnimation.cs
@@ -17,7 +17,7 @@ public class DeleteAfterAnimation : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Destroy(animator.gameObject);  
+        animator.enabled = false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
